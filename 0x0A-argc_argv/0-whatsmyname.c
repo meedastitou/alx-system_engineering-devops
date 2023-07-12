dbca7f9d046d64faa308a2228d283a43121c9015
meed astitou <astitoumd@gmail.com>
2023-07-12 12:27:58 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name of the programme
+ * @argc - number of arguments
+ * @argb - array of arguments
+ * Return - 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
